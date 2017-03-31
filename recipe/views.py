3d581df5697f8d94629d59627723c248ba769b68
Ashutosh Das <git.pyprism@gmail.com>
2017-04-01 01:55:27 +0600

    wip

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
+from django.contrib import messages
 
-# Create your views here.
+
+def login(request):
+	if request.user.is_authenticated:
+		return redirect('recipes')
+    if request.method == "POST":
+    	username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            auth.login(request, user)
+            return redirect('recipes')
+        else:
+            messages.error(request, 'Username/Password is not valid!')
+            return redirect('/')
+    else:
+        return render(request, 'login.html')
