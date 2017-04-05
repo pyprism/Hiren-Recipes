31721b519c7b2df1d3db1de4fd5c0dc8a421a495
Ashutosh Das <git.pyprism@gmail.com>
2017-04-05 10:41:12 +0600

    Added form

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,1 +1,26 @@
 from django.forms import ModelForm
+from .models import Recipe, Ingredient, CookedAt, Direction
+
+
+class RecipeForm(ModelForm):
+    class Meta:
+        model = Recipe
+        exclude = ('ingredient', 'direction')
+
+
+class IngredientForm(ModelForm):
+    class Meta:
+        model = Ingredient
+        fields = '__all__'
+
+
+class CookedAtForm(ModelForm):
+    class Meta:
+        model = CookedAt
+        exclude = 'recipe'
+
+
+class DirectionForm(ModelForm):
+    class Meta:
+        model = Direction
+        fields = "__all__"
