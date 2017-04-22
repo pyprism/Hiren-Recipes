4a6931b2aca94d87b9795967bd813c7bc15eab2a
Ashutosh Das <git.pyprism@gmail.com>
2017-04-22 12:50:16 +0600

    wip

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
-
-# Create your tests here.
+from django.core.urlresolvers import resolve
+from django.test import TestCase, TransactionTestCase
+from django.contrib.auth.models import User
+from django.test import Client
+from .models import Recipe, CookedAt
+from . import views
+from freezegun import freeze_time
